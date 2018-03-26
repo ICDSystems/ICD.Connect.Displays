c6ca1bd09ffbc279e409fccd2c8db5d44356269b
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-26 12:42:10 -0400

    Removing api attributes

--- ICD.Connect.Displays/Proxies/ProxyDisplayWithAudio.cs
+++ ICD.Connect.Displays/Proxies/ProxyDisplayWithAudio.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using ICD.Common.Utils.EventArguments;
-using ICD.Connect.API.Attributes;
 using ICD.Connect.API.Commands;
 using ICD.Connect.API.Nodes;
 using ICD.Connect.Displays.Devices;
@@ -14,58 +13,45 @@ namespace ICD.Connect.Displays.Proxies
 
 		public event EventHandler<BoolEventArgs> OnMuteStateChanged;
 
-		[ApiProperty("Volume", "Gets the volume of the display.")]
 		public float Volume { get; private set; }
 
-		[ApiProperty("IsMuted", "Gets the muted state of the display.")]
 		public bool IsMuted { get; private set; }
 
-		[ApiProperty("VolumeDeviceMin", "Gets the min volume of the display.")]
 		public float VolumeDeviceMin { get; private set; }
 
-		[ApiProperty("VolumeDeviceMax", "Gets the max volume of the display.")]
 		public float VolumeDeviceMax { get; private set; }
 
-		[ApiProperty("VolumeSafetyMin", "Gets/sets the min safety volume.")]
 		public float? VolumeSafetyMin { get; set; }
 
-		[ApiProperty("VolumeSafetyMax", "Gets/sets the max safety volume.")]
 		public float? VolumeSafetyMax { get; set; }
 
-		[ApiProperty("VolumeDefault", "Gets/sets the default volume.")]
 		public float? VolumeDefault { get; set; }
 
-		[ApiMethod("SetVolume", "Sets the display volume.")]
 		public void SetVolume(float raw)
 		{
 			throw new NotImplementedException();
 		}
 
-		[ApiMethod("VolumeUpIncrement", "Increments the display volume.")]
 		public void VolumeUpIncrement()
 		{
 			throw new NotImplementedException();
 		}
 
-		[ApiMethod("VolumeDownIncrement", "Decrements the display volume.")]
 		public void VolumeDownIncrement()
 		{
 			throw new NotImplementedException();
 		}
 
-		[ApiMethod("MuteOn", "Mutes the display.")]
 		public void MuteOn()
 		{
 			throw new NotImplementedException();
 		}
 
-		[ApiMethod("MuteOff", "Unmutes the display.")]
 		public void MuteOff()
 		{
 			throw new NotImplementedException();
 		}
 
-		[ApiMethod("MuteToggle", "Toggles the display mute state.")]
 		public void MuteToggle()
 		{
 			throw new NotImplementedException();
