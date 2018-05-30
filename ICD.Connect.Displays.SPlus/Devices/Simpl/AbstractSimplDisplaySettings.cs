2938502f3f5ff0e94388447960ee37df6754e276
Drew Tingen <drew.tingen@icdsystems.com>
2018-05-30 14:25:59 -0400

    feat: adding input count to simpl display settings

--- ICD.Connect.Displays.SPlus/Devices/Simpl/AbstractSimplDisplaySettings.cs
+++ ICD.Connect.Displays.SPlus/Devices/Simpl/AbstractSimplDisplaySettings.cs
@@ -1,8 +1,36 @@
-﻿using ICD.Connect.Devices.Simpl;
+﻿using ICD.Common.Properties;
+using ICD.Common.Utils.Xml;
+using ICD.Connect.Devices.Simpl;
 
 namespace ICD.Connect.Displays.SPlus.Devices.Simpl
 {
 	public abstract class AbstractSimplDisplaySettings : AbstractSimplDeviceSettings
 	{
+		private const string INPUT_COUNT_ELEMENT = "InputCount";
+
+		[PublicAPI]
+		public int InputCount { get; set; }
+
+		/// <summary>
+		/// Write settings elements to xml.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void WriteElements(IcdXmlTextWriter writer)
+		{
+			base.WriteElements(writer);
+
+			writer.WriteElementString(INPUT_COUNT_ELEMENT, IcdXmlConvert.ToString(InputCount));
+		}
+
+		/// <summary>
+		/// Updates the settings from xml.
+		/// </summary>
+		/// <param name="xml"></param>
+		public override void ParseXml(string xml)
+		{
+			base.ParseXml(xml);
+
+			InputCount = XmlUtils.ReadChildElementContentAsInt(xml, INPUT_COUNT_ELEMENT);
+		}
 	}
 }
