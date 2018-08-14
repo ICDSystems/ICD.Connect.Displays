4412560f5f493f61348130204e938434ab066b32
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-08-14 10:20:46 -0400

    feat: create panasonic classic driver. Works for Serial Right Now, needs Custom Serial Buffer for TCP Control

---
+++ ICD.Connect.Displays.PanasonicClassic/Devices/PanasonicClassicDisplaySettings.cs
@@ -0,0 +1,10 @@
+﻿using ICD.Connect.Displays.Settings;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Displays.PanasonicClassic.Devices
+{
+	[KrangSettings("PanasonicClassicDisplay", typeof(PanasonicClassicDisplay))]
+	public sealed class PanasonicClassicDisplaySettings : AbstractDisplayWithAudioSettings
+	{
+	}
+}
