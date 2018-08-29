6b948eb9934221f0980db72bc46875e192097323
Chris Cameron <chris.cameron@wedoresi.com>
2018-08-29 10:35:35 -0400

    feat: Initial commit of SharpPnDisplay

---
+++ ICD.Connect.Displays.Sharp/Devices/PN/SharpPnDisplaySettings.cs
@@ -0,0 +1,13 @@
+﻿using ICD.Connect.Displays.Settings;
+using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Displays.Sharp.Devices.PN
+{
+	/// <summary>
+	/// Settings for the SharpDisplay device.
+	/// </summary>
+	[KrangSettings("SharpPnDisplay", typeof(SharpPnDisplay))]
+	public sealed class SharpPnDisplaySettings : AbstractDisplayWithAudioSettings
+	{
+	}
+}
