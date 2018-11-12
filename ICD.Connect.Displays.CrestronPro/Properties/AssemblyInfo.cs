dc82555fdf8c815cea0b896f76620659e4802034
Chris Cameron <chris.cameron@wedoresi.com>
2018-11-12 11:29:21 -0500

    feat: Begin implementing RoomViewConnectedDisplayAdapter

---
+++ ICD.Connect.Displays.CrestronPro/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Displays.CrestronPro")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Connect.Displays.CrestronPro")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyVersion("9.0.0.0")]
+
---
+++ ICD.Connect.Displays.CrestronPro/RoomViewConnectedDisplayAdapter.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Devices;
+using ICD.Connect.Displays.Devices;
+using ICD.Connect.Displays.EventArguments;
+
+namespace ICD.Connect.Displays.CrestronPro
+{
+	public sealed class RoomViewConnectedDisplayAdapter : AbstractDevice<RoomViewConnectedDisplaySettings>, IDisplayWithAudio
+	{
+		/// <summary>
+		/// Raised when the power state changes.
+		/// </summary>
+		public event EventHandler<DisplayPowerStateApiEventArgs> OnIsPoweredChanged;
+
+		/// <summary>
+		/// Raised when the selected HDMI input changes.
+		/// </summary>
+		public event EventHandler<DisplayHmdiInputApiEventArgs> OnHdmiInputChanged;
+
+		/// <summary>
+		/// Raised when the scaling mode changes.
+		/// </summary>
+		public event EventHandler<DisplayScalingModeApiEventArgs> OnScalingModeChanged;
+
+		/// <summary>
+		/// Raised when the volume changes.
+		/// </summary>
+		public event EventHandler<DisplayVolumeApiEventArgs> OnVolumeChanged;
+
+		/// <summary>
+		/// Raised when the mute state changes.
+		/// </summary>
+		public event EventHandler<DisplayMuteApiEventArgs> OnMuteStateChanged;
+
+		#region Properties
+
+		/// <summary>
+		/// When true assume TX is successful even if a request times out.
+		/// </summary>
+		public bool Trust { get { return false; } set { throw new NotSupportedException(); } }
+
+		/// <summary>
+		/// Gets the powered state.
+		/// </summary>
+		public bool IsPowered { get; private set; }
+
+		/// <summary>
+		/// Gets the number of HDMI inputs.
+		/// </summary>
+		public int InputCount { get; private set; }
+
+		/// <summary>
+		/// Gets the Hdmi input.
+		/// </summary>
+		public int? HdmiInput { get; private set; }
+
+		/// <summary>
+		/// Gets the scaling mode.
+		/// </summary>
+		public eScalingMode ScalingMode { get; private set; }
+
+		/// <summary>
+		/// Gets the current volume.
+		/// </summary>
+		public float Volume { get; private set; }
+
+		/// <summary>
+		/// Gets the muted state.
+		/// </summary>
+		public bool IsMuted { get; private set; }
+
+		/// <summary>
+		/// The min volume.
+		/// </summary>
+		public float VolumeDeviceMin { get; private set; }
+
+		/// <summary>
+		/// The max volume.
+		/// </summary>
+		public float VolumeDeviceMax { get; private set; }
+
+		/// <summary>
+		/// Prevents the device from going below this volume.
+		/// </summary>
+		public float? VolumeSafetyMin { get; set; }
+
+		/// <summary>
+		/// Prevents the device from going above this volume.
+		/// </summary>
+		public float? VolumeSafetyMax { get; set; }
+
+		/// <summary>
+		/// The volume the device is set to when powered.
+		/// </summary>
+		public float? VolumeDefault { get; set; }
+
+		#endregion
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public RoomViewConnectedDisplayAdapter()
+		{
+			Controls.Add(new DisplayRouteDestinationControl(this, 0));
+			Controls.Add(new DisplayPowerDeviceControl(this, 1));
+			Controls.Add(new DisplayVolumeDeviceControl(this, 2));
+		}
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		protected override void DisposeFinal(bool disposing)
+		{
+			OnIsPoweredChanged = null;
+			OnHdmiInputChanged = null;
+			OnScalingModeChanged = null;
+			OnVolumeChanged = null;
+			OnMuteStateChanged = null;
+
+			base.DisposeFinal(disposing);
+		}
+
+		#region Methods
+
+		/// <summary>
+		/// Powers the TV.
+		/// </summary>
+		public void PowerOn()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Shuts down the TV.
+		/// </summary>
+		public void PowerOff()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Sets the Hdmi index of the TV, e.g. 1 = HDMI-1.
+		/// </summary>
+		/// <param name="address"></param>
+		public void SetHdmiInput(int address)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Sets the scaling mode.
+		/// </summary>
+		/// <param name="mode"></param>
+		public void SetScalingMode(eScalingMode mode)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Sets the raw volume.
+		/// </summary>
+		/// <param name="raw"></param>
+		public void SetVolume(float raw)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Increments the volume once.
+		/// </summary>
+		public void VolumeUpIncrement()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Decrements the volume once.
+		/// </summary>
+		public void VolumeDownIncrement()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Mutes the display.
+		/// </summary>
+		public void MuteOn()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Unmutes the display.
+		/// </summary>
+		public void MuteOff()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Toggles the mute state of the display.
+		/// </summary>
+		public void MuteToggle()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Gets the current online status of the device.
+		/// </summary>
+		/// <returns></returns>
+		protected override bool GetIsOnlineStatus()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		#region Settings
+
+
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Gets the child console nodes.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleNodeBase> GetConsoleNodes()
+		{
+			foreach (IConsoleNodeBase node in GetBaseConsoleNodes())
+				yield return node;
+
+			foreach (IConsoleNodeBase node in DisplayConsole.GetConsoleNodes(this))
+				yield return node;
+
+			foreach (IConsoleNodeBase node in DisplayWithAudioConsole.GetConsoleNodes(this))
+				yield return node;
+		}
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			DisplayConsole.BuildConsoleStatus(this, addRow);
+			DisplayWithAudioConsole.BuildConsoleStatus(this, addRow);
+		}
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			foreach (IConsoleCommand command in DisplayConsole.GetConsoleCommands(this))
+				yield return command;
+
+			foreach (IConsoleCommand command in DisplayWithAudioConsole.GetConsoleCommands(this))
+				yield return command;
+		}
+
+		/// <summary>
+		/// Workaround for the "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleNodeBase> GetBaseConsoleNodes()
+		{
+			return base.GetConsoleNodes();
+		}
+
+		#endregion
+	}
+}
