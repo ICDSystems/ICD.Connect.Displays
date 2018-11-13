cfab1fd6de1817ae9b43792324fbe77d6ce2da59
Chris Cameron <chris.cameron@wedoresi.com>
2018-11-13 14:28:31 -0500

    chore: Moving PanasonicClassic display into existing Panasonic project

--- ICD.Connect.Displays.PanasonicClassic/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("ICD.Connect.Displays.Panasonic")]
-[assembly: AssemblyCompany("ICD Systems")]
-[assembly: AssemblyProduct("ICD.Connect.Displays.Panasonic")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("10.0.0.0")]
