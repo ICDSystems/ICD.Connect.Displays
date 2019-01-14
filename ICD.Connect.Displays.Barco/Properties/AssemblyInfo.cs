799967ab6bf53f4d33316cd348c77b3bc746e2dc
Drew Tingen <drew.tingen@icdsystems.com>
2019-01-14 18:18:28 -0500

    chore: Update copyright year

--- ICD.Connect.Displays.Barco/Properties/AssemblyInfo.cs
+++ ICD.Connect.Displays.Barco/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("ICD.Connect.Displays.Barco")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ICD.Connect.Displays.Barco")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyVersion("11.0.0.0")]
 
