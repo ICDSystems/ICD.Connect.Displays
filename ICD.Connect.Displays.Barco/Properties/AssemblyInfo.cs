88bf3a34e0d58092ba851ffe9e2b60d2dc85e9e7
Chris Cameron <chris.cameron@wedoresi.com>
2019-01-15 10:23:49 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Displays.Barco/Properties/AssemblyInfo.cs
+++ ICD.Connect.Displays.Barco/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.Displays.Barco")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Displays.Barco")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
 [assembly: AssemblyVersion("11.0.0.0")]
 
