f1f4a4c461c96840e642f554faa6bb0a2fa542e9
Chris Cameron <chris.cameron@wedoresi.com>
2019-01-24 16:22:54 -0500

    feat: First pass at LgDigitalSignageDisplay, still need to handle feedback

---
+++ ICD.Connect.Displays.LG/DigitalSignage/LgDigitalSignageDisplay.cs
@@ -0,0 +1,293 @@
+﻿using ICD.Common.Utils;
+using ICD.Common.Utils.Collections;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Displays.Devices;
+using ICD.Connect.Displays.EventArguments;
+using ICD.Connect.Protocol.EventArguments;
+using ICD.Connect.Protocol.Ports;
+using ICD.Connect.Protocol.SerialBuffers;
+using ICD.Connect.Protocol.SerialQueues;
+using ICD.Connect.Settings;
+
+namespace ICD.Connect.Displays.LG.DigitalSignage
+{
+	public sealed class LgDigitalSignageDisplay : AbstractDisplayWithAudio<LgDigitalSignageDisplaySettings>
+	{
+		/// <summary>
+		/// Maps scaling mode to command.
+		/// </summary>
+		private static readonly BiDictionary<eScalingMode, string> s_ScalingModeMap =
+			new BiDictionary<eScalingMode, string>
+			{
+				{eScalingMode.Wide16X9, "02"},
+				{eScalingMode.Square4X3, "01"},
+				{eScalingMode.NoScale, "09"},
+				{eScalingMode.Zoom, "04"}
+			};
+
+		#region Properties
+
+		public int SetId { get; set; }
+
+		#endregion
+
+		public override void ConfigurePort(ISerialPort port)
+		{
+			base.ConfigurePort(port);
+
+			if (port != null)
+			{
+				port.DebugRx = eDebugMode.MixedAsciiHex;
+				port.DebugTx = eDebugMode.MixedAsciiHex;
+			}
+
+			ISerialBuffer buffer = new DelimiterSerialBuffer((char)0x0D);
+			SerialQueue queue = new SerialQueue();
+			queue.SetPort(port);
+			queue.SetBuffer(buffer);
+			queue.Timeout = 10 * 1000;
+
+			SetSerialQueue(queue);
+		}
+
+		#region Methods
+
+		/// <summary>
+		/// Powers the TV.
+		/// </summary>
+		public override void PowerOn()
+		{
+			LgDigitalSignageTransmission command = new LgDigitalSignageTransmission
+			{
+				Command1 = 'k',
+				Command2 = 'a',
+				SetId = SetId,
+				Data = "01"
+			};
+
+			SendCommand(command);
+		}
+
+		/// <summary>
+		/// Shuts down the TV.
+		/// </summary>
+		public override void PowerOff()
+		{
+			LgDigitalSignageTransmission command = new LgDigitalSignageTransmission
+			{
+				Command1 = 'k',
+				Command2 = 'a',
+				SetId = SetId,
+				Data = "00"
+			};
+
+			SendCommand(command);
+		}
+
+		/// <summary>
+		/// Sets the Hdmi index of the TV, e.g. 1 = HDMI-1.
+		/// </summary>
+		/// <param name="address"></param>
+		public override void SetActiveInput(int address)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		/// Sets the scaling mode.
+		/// </summary>
+		/// <param name="mode"></param>
+		public override void SetScalingMode(eScalingMode mode)
+		{
+			LgDigitalSignageTransmission command = new LgDigitalSignageTransmission
+			{
+				Command1 = 'k',
+				Command2 = 'c',
+				SetId = SetId,
+				Data = s_ScalingModeMap.GetValue(mode)
+			};
+
+			SendCommand(command);
+		}
+
+		/// <summary>
+		/// Increments the raw volume.
+		/// </summary>
+		public override void VolumeUpIncrement()
+		{
+			VolumeSetRawFinal(Volume + 1);
+		}
+
+		/// <summary>
+		/// Decrements the raw volume.
+		/// </summary>
+		public override void VolumeDownIncrement()
+		{
+			VolumeSetRawFinal(Volume - 1);
+		}
+
+		/// <summary>
+		/// Sends the volume set command to the device after validation has been performed.
+		/// </summary>
+		/// <param name="raw"></param>
+		protected override void VolumeSetRawFinal(float raw)
+		{
+			int volumeInt = (int)MathUtils.Clamp(raw, VolumeDeviceMin, VolumeDeviceMax);
+
+			LgDigitalSignageTransmission command = new LgDigitalSignageTransmission
+			{
+				Command1 = 'k',
+				Command2 = 'f',
+				SetId = SetId,
+				Data = volumeInt.ToString("X2") // 2 digit hex (00-64)
+			};
+
+			SendCommand(command);
+		}
+
+		/// <summary>
+		/// Enables mute.
+		/// </summary>
+		public override void MuteOn()
+		{
+			LgDigitalSignageTransmission command = new LgDigitalSignageTransmission
+			{
+				Command1 = 'k',
+				Command2 = 'e',
+				SetId = SetId,
+				Data = "00"
+			};
+
+			SendCommand(command);
+		}
+
+		/// <summary>
+		/// Disables mute.
+		/// </summary>
+		public override void MuteOff()
+		{
+			LgDigitalSignageTransmission command = new LgDigitalSignageTransmission
+			{
+				Command1 = 'k',
+				Command2 = 'e',
+				SetId = SetId,
+				Data = "01"
+			};
+
+			SendCommand(command);
+		}
+
+		#endregion
+
+		#region Serial Queue Callbacks
+
+		/// <summary>
+		/// Called when a command is sent to the physical display.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		protected override void SerialQueueOnSerialTransmission(object sender, SerialTransmissionEventArgs args)
+		{
+			//IcdConsole.PrintLine(eConsoleColor.Magenta, "SerialQueueOnSerialTransmission {0}", args.Data);
+		}
+
+		/// <summary>
+		/// Called when a command gets a response from the physical display.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		protected override void SerialQueueOnSerialResponse(object sender, SerialResponseEventArgs args)
+		{
+			LgDigitalSignageAcknowledgement acknowledgement;
+			if (!LgDigitalSignageAcknowledgement.Deserialize(args.Response, out acknowledgement))
+				return;
+
+			//IcdConsole.PrintLine(eConsoleColor.Magenta, "SerialQueueOnSerialResponse {0} - {1}", args.Data, args.Response);
+		}
+
+		/// <summary>
+		/// Called when a command times out.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		protected override void SerialQueueOnTimeout(object sender, SerialDataEventArgs args)
+		{
+			IcdConsole.PrintLine(eConsoleColor.Magenta, "SerialQueueOnTimeout {0}", args.Data);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Polls the physical device for the current state.
+		/// </summary>
+		protected override void QueryState()
+		{
+			base.QueryState();
+
+			/*
+			// Query the state of the device
+			SendCommand(new SamsungProCommand(POWER, WallId, 0).ToQuery());
+
+			if (!IsPowered)
+				return;
+
+			SendCommand(new SamsungProCommand(VOLUME, WallId, 0).ToQuery());
+			SendCommand(new SamsungProCommand(INPUT, WallId, 0).ToQuery());
+			SendCommand(new SamsungProCommand(SCREEN_MODE, WallId, 0).ToQuery());
+			SendCommand(new SamsungProCommand(MUTE, WallId, 0).ToQuery());
+			 */
+		}
+
+		/// <summary>
+		/// Override to clear the instance settings.
+		/// </summary>
+		protected override void ClearSettingsFinal()
+		{
+			base.ClearSettingsFinal();
+
+			SetId = 1;
+		}
+
+		/// <summary>
+		/// Override to apply properties to the settings instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		protected override void CopySettingsFinal(LgDigitalSignageDisplaySettings settings)
+		{
+			base.CopySettingsFinal(settings);
+
+			settings.SetId = SetId;
+		}
+
+		/// <summary>
+		/// Override to apply settings to the instance.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="factory"></param>
+		protected override void ApplySettingsFinal(LgDigitalSignageDisplaySettings settings, IDeviceFactory factory)
+		{
+			base.ApplySettingsFinal(settings, factory);
+
+			SetId = settings.SetId;
+		}
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			addRow("Set ID", SetId);
+		}
+
+		#endregion
+	}
+}
---
+++ ICD.Connect.Displays.LG/DigitalSignage/LgDigitalSignageTransmission.cs
@@ -0,0 +1,114 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using ICD.Common.Utils;
+using ICD.Connect.Protocol.Data;
+
+namespace ICD.Connect.Displays.LG.DigitalSignage
+{
+	/// <summary>
+	/// [Command1][Command2][ ][Set ID][ ][Data][Cr]
+	/// 
+	/// E.g. Power On: "ka 01 01\x0D"
+	/// 
+	/// * [Command1]: identifies between the factory setting and the user setting modes.
+	/// * [Command2]: controls monitor sets.
+	/// * [Set ID]: Used for selecting a set you want to control. A unique Set ID can be assigned to each set ranging
+	///             from 1 to 1000(01H~3E8H) under Settings in the OSD menu.
+	///	            Selecting '00H' for Set ID allows the simultaneous control of all connected monitors.
+	///	            (The maximum value may differ depending on the model.)
+	/// * [Data]: Transmits command data.
+	///	          Data count may increase depending on the command.
+	/// * [Cr]: Carriage Return. Corresponds to '0x0D' in ASCII code.
+	/// * [ ]: White Space. Corresponds to '0x20' in ASCII code
+	/// </summary>
+	public sealed class LgDigitalSignageTransmission : ISerialData
+	{
+		public const string QUERY = "\0xFF";
+
+		public char Command1 { get; set; }
+
+		public char Command2 { get; set; }
+
+		public int SetId { get; set; }
+
+		public string Data { get; set; }
+
+		/// <summary>
+		/// Serialize this instance to a string.
+		/// </summary>
+		/// <returns></returns>
+		public string Serialize()
+		{
+			return new StringBuilder()
+				.Append(Command1)
+				.Append(Command2)
+				.Append(' ')
+				.Append(SetId)
+				.Append(' ')
+				.Append(Data)
+				.Append((char)0x0D)
+				.ToString();
+		}
+
+		public override string ToString()
+		{
+			return Serialize();
+		}
+	}
+
+	/// <summary>
+	/// [Command2][ ][Set ID][ ][OK/NG][Data][x]
+	/// 
+	/// * The Product transmits ACK (acknowledgement) based on this format when receiving normal data. At this
+	/// time, if the data is FF, it indicates the present status data. If the data is in data write mode, it returns the data of
+	/// the PC computer.
+	/// * If a command is sent with Set ID '00' (=0x00), the data is reflected to all monitor sets and each monitor set
+	/// does not send an acknowledgement (ACK).
+	/// * If the data value 'FF' is sent in control mode via RS-232C, the current setting value of a function can be
+	/// checked (only for some functions).
+	/// *Some commands are not supported depending on the model.
+	/// </summary>
+	public sealed class LgDigitalSignageAcknowledgement
+	{
+		public enum eAck
+		{
+			Ok,
+			Ng
+		}
+
+		private const string ACK_REGEX = @"(?'command2'\S) (?'setId'\d+) (?'ack'OK|NG)(?'data'[0-9a-fA-F]+)x";
+
+		public char Command2 { get; private set; }
+
+		public int SetId { get; private set; }
+
+		public eAck Ack { get; private set; }
+
+		public string Data { get; private set; }
+
+		/// <summary>
+		/// Attempts to deserialize the given data string into an acknowledgement.
+		/// </summary>
+		/// <param name="data"></param>
+		/// <param name="output"></param>
+		/// <returns></returns>
+		public static bool Deserialize(string data, out LgDigitalSignageAcknowledgement output)
+		{
+			output = null;
+
+			Match match = Regex.Match(data, ACK_REGEX);
+			if (!match.Success)
+				return false;
+
+			output = new LgDigitalSignageAcknowledgement
+			{
+				Command2 = match.Groups["command2"].Value[0],
+				SetId = int.Parse(match.Groups["setId"].Value),
+				Ack = EnumUtils.Parse<eAck>(match.Groups["ack"].Value, true),
+				Data = match.Groups["data"].Value
+			};
+
+			return true;
+		}
+	}
+}
