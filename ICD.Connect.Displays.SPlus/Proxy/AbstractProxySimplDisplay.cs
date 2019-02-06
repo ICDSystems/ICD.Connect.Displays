cc1021d72d4d60219d3214cb43536a316fd1d3b7
Drew Tingen <drew.tingen@icdsystems.com>
2019-02-06 09:36:01 -0800

    feat: Simpl Display Proxy devices

--- ICD.Connect.Displays.SPlus/Proxy/AbstractProxySimplDisplay.cs
+++ ICD.Connect.Displays.SPlus/Proxy/AbstractProxySimplDisplay.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using ICD.Common.Utils.Extensions;
+using ICD.Connect.API;
+using ICD.Connect.API.Info;
 using ICD.Connect.Devices.Simpl;
 using ICD.Connect.Displays.EventArguments;
 using ICD.Connect.Displays.SPlus.Devices.Simpl;
@@ -12,19 +15,88 @@ namespace ICD.Connect.Displays.SPlus.Proxy
 		public event EventHandler<SetPowerApiEventArgs> OnSetPower;
 		public event EventHandler<SetActiveInputApiEventArgs> OnSetActiveInput;
 		public event EventHandler<SetScalingModeEventArgs> OnSetScalingMode;
+
+		#region Public Methods
+		
 		public void SetPowerFeedback(bool isPowered)
 		{
-			throw new NotImplementedException();
+			CallMethod(SPlusDisplayApi.METHOD_SET_POWER_FEEDBACK, isPowered);
 		}
 
 		public void SetActiveInputFeedback(int? address)
 		{
-			throw new NotImplementedException();
+			CallMethod(SPlusDisplayApi.METHOD_SET_ACTIVE_INPUT_FEEDBACK, address);
 		}
 
 		public void SetScalingModeFeedback(eScalingMode mode)
 		{
-			throw new NotImplementedException();
+			CallMethod(SPlusDisplayApi.METHOD_SET_SCALING_MODE_FEEDBACK, mode);
 		}
+
+		#endregion
+
+		#region API
+
+		/// <summary>
+		/// Override to build initialization commands on top of the current class info.
+		/// </summary>
+		/// <param name="command"></param>
+		protected override void Initialize(ApiClassInfo command)
+		{
+			base.Initialize(command);
+
+			ApiCommandBuilder.UpdateCommand(command)
+			                 .SubscribeEvent(SPlusDisplayApi.EVENT_SET_POWER)
+			                 .SubscribeEvent(SPlusDisplayApi.EVENT_SET_ACTIVE_INPUT)
+			                 .SubscribeEvent(SPlusDisplayApi.EVENT_SET_SCALING_MODE)
+			                 .Complete();
+		}
+
+		/// <summary>
+		/// Updates the proxy with event feedback info.
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="result"></param>
+		protected override void ParseEvent(string name, ApiResult result)
+		{
+			base.ParseEvent(name, result);
+
+			switch (name)
+			{
+				case SPlusDisplayApi.EVENT_SET_POWER:
+					bool powerState = result.GetValue<bool>();
+					RaiseSetPower(powerState);
+					break;
+				case SPlusDisplayApi.EVENT_SET_ACTIVE_INPUT:
+					int activeInput = result.GetValue<int>();
+					RaiseSetActiveInput(activeInput);
+					break;
+				case SPlusDisplayApi.EVENT_SET_SCALING_MODE:
+					eScalingMode mode = result.GetValue<eScalingMode>();
+					RaiseSetScalingMode(mode);
+					break;
+			}
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void RaiseSetPower(bool powerState)
+		{
+			OnSetPower.Raise(this, new SetPowerApiEventArgs(powerState));
+		}
+
+		private void RaiseSetActiveInput(int activeInput)
+		{
+			OnSetActiveInput.Raise(this, new SetActiveInputApiEventArgs(activeInput));
+		}
+
+		private void RaiseSetScalingMode(eScalingMode mode)
+		{
+			OnSetScalingMode.Raise(this, new SetScalingModeEventArgs(mode));
+		}
+
+		#endregion
 	}
 }
--- ICD.Connect.Displays.SPlus/Proxy/ProxySimplDisplayWithAudio.cs
+++ ICD.Connect.Displays.SPlus/Proxy/ProxySimplDisplayWithAudio.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using ICD.Common.Utils.Extensions;
+using ICD.Connect.API;
+using ICD.Connect.API.Info;
 using ICD.Connect.Displays.SPlus.Devices.Simpl;
 using ICD.Connect.Displays.SPlus.EventArgs;
 
@@ -10,14 +13,92 @@ namespace ICD.Connect.Displays.SPlus.Proxy
 		public event EventHandler<SetVolumeIncrementApiEventArgs> OnSetVolumeIncrement;
 		public event EventHandler<SetMuteApiEventArgs> OnSetMute;
 		public event EventHandler<SetMuteToggleApiEventArgs> OnSetMuteToggle;
+
+		#region Methods
+
 		public void SetVolumeFeedback(float volume)
 		{
-			throw new NotImplementedException();
+			CallMethod(SPlusDisplayApi.METHOD_SET_VOLUME_FEEDBACK, volume);
 		}
 
 		public void SetMuteFeedback(bool mute)
 		{
-			throw new NotImplementedException();
+			CallMethod(SPlusDisplayApi.METHOD_SET_MUTE_FEEDBACK, mute);
 		}
+
+		#endregion
+
+		#region API
+
+		/// <summary>
+		/// Override to build initialization commands on top of the current class info.
+		/// </summary>
+		/// <param name="command"></param>
+		protected override void Initialize(ApiClassInfo command)
+		{
+			base.Initialize(command);
+
+			ApiCommandBuilder.UpdateCommand(command)
+							 .SubscribeEvent(SPlusDisplayApi.EVENT_SET_VOLUME)
+							 .SubscribeEvent(SPlusDisplayApi.EVENT_SET_VOLUME_INCREMENT)
+							 .SubscribeEvent(SPlusDisplayApi.EVENT_SET_MUTE)
+							 .SubscribeEvent(SPlusDisplayApi.EVENT_SET_MUTE_TOGGLE)
+							 .Complete();
+		}
+
+		/// <summary>
+		/// Updates the proxy with event feedback info.
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="result"></param>
+		protected override void ParseEvent(string name, ApiResult result)
+		{
+			base.ParseEvent(name, result);
+
+			switch (name)
+			{
+				case SPlusDisplayApi.EVENT_SET_VOLUME:
+					float volume = result.GetValue<float>();
+					RaiseSetVolume(volume);
+					break;
+				case SPlusDisplayApi.EVENT_SET_VOLUME_INCREMENT:
+					bool direction = result.GetValue<bool>();
+					RaiseSetVolumeIncrement(direction);
+					break;
+				case SPlusDisplayApi.EVENT_SET_MUTE:
+					bool mute = result.GetValue<bool>();
+					RaiseSetMute(mute);
+					break;
+				case SPlusDisplayApi.EVENT_SET_MUTE_TOGGLE:
+					RaiseSetMuteToggle();
+					break;
+			}
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void RaiseSetVolume(float volume)
+		{
+			OnSetVolume.Raise(this, new SetVolumeApiEventArgs(volume));
+		}
+
+		private void RaiseSetVolumeIncrement(bool direction)
+		{
+			OnSetVolumeIncrement.Raise(this, new SetVolumeIncrementApiEventArgs(direction));
+		}
+
+		private void RaiseSetMute(bool mute)
+		{
+			OnSetMute.Raise(this, new SetMuteApiEventArgs(mute));
+		}
+
+		private void RaiseSetMuteToggle()
+		{
+			OnSetMuteToggle.Raise(this, new SetMuteToggleApiEventArgs());
+		}
+
+		#endregion
 	}
 }
