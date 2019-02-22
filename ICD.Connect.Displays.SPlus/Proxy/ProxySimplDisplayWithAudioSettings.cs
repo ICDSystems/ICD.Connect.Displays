af14a7d9c94162b95ee15d2063e574b4cf4fec06
Drew Tingen <drew.tingen@icdsystems.com>
2019-02-22 12:26:51 -0800

    fix: Adding ProxySimplDisplayWithAudio Krang Originator Attribute

--- ICD.Connect.Displays.SPlus/Proxy/ProxySimplDisplayWithAudioSettings.cs
+++ ICD.Connect.Displays.SPlus/Proxy/ProxySimplDisplayWithAudioSettings.cs
@@ -1,5 +1,8 @@
-﻿namespace ICD.Connect.Displays.SPlus.Proxy
+﻿using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Displays.SPlus.Proxy
 {
+	[KrangSettings("ProxySimplDisplayWithAudio", typeof(ProxySimplDisplayWithAudio))]
 	public abstract class ProxySimplDisplayWithAudioSettings : AbstractProxySimplDisplaySettings, IProxySimplDisplayWithAudioSettings
 	{
 	}
