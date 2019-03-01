cc5f72e2faa2cd981ecd2f5e24389d720daa225c
Chris Cameron <chris.cameron@wedoresi.com>
2019-03-01 12:20:34 -0500

    fix: Fixing net standard build

--- ICD.Connect.Displays.SPlus/Proxy/ProxySimplDisplay.cs
+++ ICD.Connect.Displays.SPlus/Proxy/ProxySimplDisplay.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Crestron.SimplSharp;
-
-namespace ICD.Connect.Displays.SPlus.Proxy
+﻿namespace ICD.Connect.Displays.SPlus.Proxy
 {
 	public sealed class ProxySimplDisplay : AbstractProxySimplDisplay<ProxySimplDisplaySettings>
 	{
