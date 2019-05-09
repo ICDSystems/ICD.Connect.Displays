b4d18897cdbba307d0d014d313c848c017f30e77
Drew Tingen <drew.tingen@icdsystems.com>
2019-05-09 10:27:51 -0400

    fix:Adding KrangSetting attribute to ProxySimplDisplay

--- ICD.Connect.Displays.SPlus/Proxy/ProxySimplDisplaySettings.cs
+++ ICD.Connect.Displays.SPlus/Proxy/ProxySimplDisplaySettings.cs
@@ -1,5 +1,8 @@
-﻿namespace ICD.Connect.Displays.SPlus.Proxy
+﻿using ICD.Connect.Settings.Attributes;
+
+namespace ICD.Connect.Displays.SPlus.Proxy
 {
+	[KrangSettings("ProxySimplDisplay", typeof(ProxySimplDisplay))]
 	public sealed class ProxySimplDisplaySettings : AbstractProxySimplDisplaySettings
 	{
 	}
