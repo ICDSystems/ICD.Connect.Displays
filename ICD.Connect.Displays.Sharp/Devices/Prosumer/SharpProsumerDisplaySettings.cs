cada71453d5fbe1d2703f68b134ae369493e0ace
Chris Cameron <chris.cameron@icdsystems.com>
2019-11-09 10:03:20 -0500

    refactor: Reducing duplicate code

--- ICD.Connect.Displays.Sharp/Devices/Prosumer/SharpProsumerDisplaySettings.cs
+++ ICD.Connect.Displays.Sharp/Devices/Prosumer/SharpProsumerDisplaySettings.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using ICD.Connect.Displays.Settings;
-using ICD.Connect.Protocol.Network.Settings;
-using ICD.Connect.Protocol.Ports.ComPort;
-using ICD.Connect.Protocol.Settings;
+﻿using ICD.Connect.Displays.Sharp.Devices.Consumer;
 using ICD.Connect.Settings.Attributes;
 
 namespace ICD.Connect.Displays.Sharp.Devices.Prosumer
@@ -11,35 +7,7 @@ namespace ICD.Connect.Displays.Sharp.Devices.Prosumer
 	/// Settings for the SharpProsumerDisplay device.
 	/// </summary>
 	[KrangSettings("SharpProsumerDisplay", typeof(SharpProsumerDisplay))]
-	public sealed class SharpProsumerDisplaySettings : AbstractDisplayWithAudioSettings
+	public sealed class SharpProsumerDisplaySettings : AbstractSharpConsumerDisplaySettings
 	{
-		/// <summary>
-		/// Sets default values for unconfigured network properties.
-		/// </summary>
-		/// <param name="networkProperties"></param>
-		protected override void UpdateNetworkDefaults(SecureNetworkProperties networkProperties)
-		{
-			if (networkProperties == null)
-				throw new ArgumentNullException("networkProperties");
-		}
-
-		/// <summary>
-		/// Sets default values for unconfigured comspec properties.
-		/// </summary>
-		/// <param name="comSpecProperties"></param>
-		protected override void UpdateComSpecDefaults(ComSpecProperties comSpecProperties)
-		{
-			if (comSpecProperties == null)
-				throw new ArgumentNullException("comSpecProperties");
-
-			comSpecProperties.ApplyDefaultValues(eComBaudRates.BaudRate9600,
-												 eComDataBits.DataBits8,
-												 eComParityType.None,
-												 eComStopBits.StopBits1,
-												 eComProtocolType.Rs232,
-												 eComHardwareHandshakeType.None,
-												 eComSoftwareHandshakeType.None,
-												 false);
-		}
 	}
 }
