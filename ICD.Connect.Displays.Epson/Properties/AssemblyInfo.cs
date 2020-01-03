c48a8512cf7f18389376aa7bb417bb3c1ed9eb5b
Chris Cameron <chris.cameron@wedoresi.com>
2020-01-03 10:28:29 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Displays.Epson/Properties/AssemblyInfo.cs
+++ ICD.Connect.Displays.Epson/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Displays.Epson")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Displays.Epson")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
 [assembly: AssemblyVersion("13.0.0.0")]
--- ICD.Connect.Displays/Properties/AssemblyInfo.cs
+++ ICD.Connect.Displays/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Displays")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Displays")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
 [assembly: AssemblyVersion("13.0.0.0")]
