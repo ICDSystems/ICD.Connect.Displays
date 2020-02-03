855969e2fe07a7b2e067dd4e98891d3d0b76e140
Chris Cameron <chris.cameron@wedoresi.com>
2020-02-03 15:39:28 -0500

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Displays.Sharp/Properties/AssemblyInfo.cs
+++ ICD.Connect.Displays.Sharp/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Displays.Sharp")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Displays.Sharp")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
-[assembly: AssemblyVersion("11.4.2.0")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyVersion("11.5.0.0")]
