9aa51585ea365f0a9738bc385a4449ca4fd50e00
Chris Cameron <chris.cameron@wedoresi.com>
2020-02-14 14:51:06 -0500

    chore: Updating changelog, incrementing patch version

--- ICD.Connect.Displays.Panasonic/Properties/AssemblyInfo.cs
+++ ICD.Connect.Displays.Panasonic/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Displays.Panasonic")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
-[assembly: AssemblyVersion("11.5.0.0")]
+[assembly: AssemblyVersion("11.5.1.0")]
