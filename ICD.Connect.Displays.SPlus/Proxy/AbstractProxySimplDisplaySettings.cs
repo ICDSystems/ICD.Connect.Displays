e9fad7cec11cbd81e91b27c8fd298f6aed067045
Chris Cameron <chris.cameron@wedoresi.com>
2020-04-21 09:52:39 -0400

    fix: Implementing missing IDeviceBase members

--- ICD.Connect.Displays.SPlus/Proxy/AbstractProxySimplDisplaySettings.cs
+++ ICD.Connect.Displays.SPlus/Proxy/AbstractProxySimplDisplaySettings.cs
@@ -1,8 +1,29 @@
-﻿using ICD.Connect.Settings.Proxies;
+﻿using System;
+using ICD.Connect.Devices;
+using ICD.Connect.Settings.Proxies;
 
 namespace ICD.Connect.Displays.SPlus.Proxy
 {
 	public class AbstractProxySimplDisplaySettings : AbstractProxySettings, IProxySimplDisplaySettings
 	{
+		/// <summary>
+		/// Gets/sets the manufacturer for this device.
+		/// </summary>
+		string IDeviceBaseSettings.Manufacturer { get; set; }
+
+		/// <summary>
+		/// Gets/sets the model number for this device.
+		/// </summary>
+		string IDeviceBaseSettings.Model { get; set; }
+
+		/// <summary>
+		/// Gets/sets the serial number for this device.
+		/// </summary>
+		string IDeviceBaseSettings.SerialNumber { get; set; }
+
+		/// <summary>
+		/// Gets/sets the purchase date for this device.
+		/// </summary>
+		DateTime IDeviceBaseSettings.PurchaseDate { get; set; }
 	}
 }
