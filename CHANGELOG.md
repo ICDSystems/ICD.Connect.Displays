88d554401c5ba0cd5dd704310f7377d3f90c3e56
Drew Tingen <drew.tingen@icdsystems.com>
2020-10-02 02:21:00 -0400

    feat: implement StartSettings() to start communications

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+ - Implemented StartSettings() to start communications with devices
+ - Cleaned up AbstractProjectorScreenDevice to use new LifecycleState
 
 ## [14.0.1] - 2020-06-16
 ### Changed
--- ICD.Connect.Displays/Devices/AbstractDisplay.cs
+++ ICD.Connect.Displays/Devices/AbstractDisplay.cs
@@ -193,7 +193,7 @@ namespace ICD.Connect.Displays.Devices
 		[PublicAPI]
 		public void SetPort(ISerialPort port)
 		{
-			m_ConnectionStateManager.SetPort(port);
+			m_ConnectionStateManager.SetPort(port, false);
 		}
 
 		/// <summary>
@@ -526,6 +526,17 @@ namespace ICD.Connect.Displays.Devices
 			UpdateCachedOnlineStatus();
 
 			Trust = settings.Trust;
+		}
+
+		/// <summary>
+		/// Override to add actions on StartSettings
+		/// This should be used to start communications with devices and perform initial actions
+		/// </summary>
+		protected override void StartSettingsFinal()
+		{
+			base.StartSettingsFinal();
+
+			m_ConnectionStateManager.Start();
 		}
 
 		#endregion
